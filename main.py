0cb38d1028c7b8fd627d0fe70d757b9aaf2ba9ff
Ben Wallace <benfwalla@gmail.com>
2025-01-19 22:33:19 -0500

    Stand up secure cronjob

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Header
 from dotenv import load_dotenv
 
 from util.validate import validate_bearer_token
@@ -11,8 +11,9 @@ load_dotenv()
 PROXY_USERNAME = os.getenv("PROXY_USERNAME")
 PROXY_PASSWORD = os.getenv("PROXY_PASSWORD")
 PROXY_URL = os.getenv("PROXY_URL")
+CRON_SECRET = os.getenv("CRON_SECRET")
 
-if not all([PROXY_USERNAME, PROXY_PASSWORD, PROXY_URL]):
+if not all([PROXY_USERNAME, PROXY_PASSWORD, PROXY_URL, CRON_SECRET]):
     raise ValueError("Missing required proxy credentials in the environment variables.")
 
 # Construct the full proxy URL
@@ -26,10 +27,18 @@ app = FastAPI()
 
 
 @app.get("/getListings")
-def get_listings(perPage: int = 10, _: bool = Depends(validate_bearer_token)):
+def get_listings(perPage: int = 25, _: bool = Depends(validate_bearer_token)):
     return get_listings_util(perPage, proxies)
 
 
 @app.post("/insertListings")
-def insert_listings(perPage: int = 10, _: bool = Depends(validate_bearer_token)):
+def insert_listings(perPage: int = 25, _: bool = Depends(validate_bearer_token)):
     return insert_listings_util(perPage, proxies)
+
+@app.post("/cron/insertListings")
+def cron_insert_listings(authorization: str = Header(...), perPage: int = 25):
+    # Validate the Authorization header
+    if authorization == f"Bearer {CRON_SECRET}":
+        return insert_listings_util(perPage, proxies)
+    else:
+        raise HTTPException(status_code=401, detail="Unauthorized")
