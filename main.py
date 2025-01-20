216f7bc85d0e6090b450d2f171fb38eec7bc2276
Ben Wallace <benfwalla@gmail.com>
2025-01-19 22:48:51 -0500

    Update cron cadence

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def insert_listings(perPage: int = 25, _: bool = Depends(validate_bearer_token))
     return insert_listings_util(perPage, proxies)
 
 @app.post("/cron/insertListings")
-def cron_insert_listings(authorization: str = Header(...), perPage: int = 25):
+def cron_insert_listings(authorization: str = Header(...), perPage: int = 30):
     # Validate the Authorization header
     if authorization == f"Bearer {CRON_SECRET}":
         return insert_listings_util(perPage, proxies)
