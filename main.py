b9ab5c2407dd52c853ddb7e51cf5a06501bc8ff9
Ben Wallace <benfwalla@gmail.com>
2025-01-25 11:21:22 -0700

    Add /getBlob

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-from fastapi import FastAPI, Depends, HTTPException, Header
+import requests
+from fastapi import FastAPI, Depends, HTTPException, Header, Response
 from dotenv import load_dotenv
 
 from util.validate import validate_bearer_token
@@ -11,9 +12,8 @@ load_dotenv()
 PROXY_USERNAME = os.getenv("PROXY_USERNAME")
 PROXY_PASSWORD = os.getenv("PROXY_PASSWORD")
 PROXY_URL = os.getenv("PROXY_URL")
-CRON_SECRET = os.getenv("CRON_SECRET")
 
-if not all([PROXY_USERNAME, PROXY_PASSWORD, PROXY_URL, CRON_SECRET]):
+if not all([PROXY_USERNAME, PROXY_PASSWORD, PROXY_URL]):
     raise ValueError("Missing required proxy credentials in the environment variables.")
 
 # Construct the full proxy URL
@@ -37,3 +37,32 @@ def get_listings(perPage: int = 25, _: bool = Depends(validate_bearer_token)):
 @app.post("/insertListings")
 def insert_listings(perPage: int = 25, _: bool = Depends(validate_bearer_token)):
     return insert_listings_util(perPage, proxies)
+
+
+@app.get("/getBlob")
+def get_blob(response: Response):
+    # Blob URL
+    blob_url = "https://591qwi72as9qsy9j.public.blob.vercel-storage.com/latest_listings.json"
+
+    try:
+        # Fetch the blob content using requests
+        blob_response = requests.get(blob_url)
+
+        if blob_response.status_code != 200:
+            raise HTTPException(
+                status_code=blob_response.status_code,
+                detail=f"Failed to fetch blob: {blob_response.reason}",
+            )
+
+        # Add required CORS headers
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        response.headers["Access-Control-Allow-Methods"] = "GET,HEAD,OPTIONS"
+        response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+        response.headers["Access-Control-Allow-Credentials"] = "true"
+
+        # Return the blob content
+        return blob_response.json()
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error fetching blob: {str(e)}")
+
